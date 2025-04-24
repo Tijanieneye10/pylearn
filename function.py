87807537060e913a277fb81d3c77c30f5e5b6d4b
Tijani Usman <brainyworld10@gmail.com>
2025-04-24 16:51:32 +0100

    wip

--- function.py
+++ function.py
@@ -1,4 +1,22 @@
 def greeting(name) -> str:
     return 'Hello! {person}'.format(person=name)
 
+# default param
+def default_param(name = 'Usman') -> str:
+    return f'Hello! {name}'
+
+
+# argument
+def get_args(*param):
+    return param
+
+def user_profile(**kwargs):
+    return kwargs
+
+profile = user_profile(name = 'Usman', age = 18)
+
+print(profile['age'])
+
 print(greeting('Tijani'))
+print(default_param())
+print(get_args(1,2,3,4,'name'))
