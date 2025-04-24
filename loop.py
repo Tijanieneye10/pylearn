bb19f6e6147d0422b7ebeae6f3daa3736a54c6ae
Tijani Usman <brainyworld10@gmail.com>
2025-04-24 15:53:19 +0100

    wip

---
+++ loop.py
@@ -0,0 +1,10 @@
+list = ['apple', 'banana', 'orange']
+
+
+for item in range(18):
+    if item % 2 == 0:
+        print('is even number')
+    elif item == 15:
+        print('this is 15')
+    else:
+        print('is odd number')
