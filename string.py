bd185d01f5ce1ce269bece4ab54eba409799aa19
Tijani Usman <brainyworld10@gmail.com>
2025-04-24 13:44:37 +0100

    strings

---
+++ string.py
@@ -0,0 +1,11 @@
+# format types
+# f-string
+# %
+# .format()
+#
+user_name = "Tijani Eneye"
+
+new_name = user_name.replace('Eneye', 'Usman')
+
+greeting = 'Hello {new_name} and {user_name}'.format()
+print(greeting)
