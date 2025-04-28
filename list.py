27a9696b267b725c8d4722774cefd0e0816ed1ae
Tijani Usman <brainyworld10@gmail.com>
2025-04-28 09:45:24 +0100

    wip

---
+++ list.py
@@ -0,0 +1,18 @@
+my_list = [1,2,3,4,5]
+
+# Access using index
+print(my_list[0])
+# Insert into the list
+my_list.insert(1,6)
+print(my_list)
+# Append to this list
+my_list.append(7)
+print(my_list)
+# Remove from the list
+my_list.remove(2)
+print(my_list)
+# Pop from the list
+my_list.pop()
+print(my_list)
+# Find using index
+print(my_list.index(4))
